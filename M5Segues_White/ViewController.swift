3c68d9a60caf0e637162c022e4e409693702b4f8
james_white249 <james_white249@mymail.eku.edu>
2026-02-19 11:08:38 -0500

    Initial Commit

---
+++ M5Segues_White/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M5Segues_White
+//
+//  Created by James White on 2/19/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
